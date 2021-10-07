7b8432bc2c061e4b0366e39370df392e2657c949
Jakub <bodziov3@gmail.com>
2021-10-07 15:41:57 +0200

    Inicjalizacja projektu

---
+++ src/main/java/com/korepetycjespring/KorepetycjeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.korepetycjespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorepetycjeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorepetycjeSpringApplication.class, args);
+    }
+
+}
